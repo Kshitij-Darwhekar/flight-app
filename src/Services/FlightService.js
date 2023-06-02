11bbc0f11f4e39ed1693a152ae43bbd850a3c813
kshitij <kshitijdarwhekar@gmail.com>
2023-06-02 09:55:33 +0530

    Flight Service

---
+++ src/Services/FlightService.js
@@ -0,0 +1,28 @@
+
+
+export default class FlightService {
+    constructor() {
+        this.flights = [
+            {code:432,carrier:'Air India',source:'Mumbai',destination:'Delhi'},
+            {code:433,carrier:'Indigo',source:'Mumbai',destination:'Pune'}
+        ]
+    }
+
+    addFlight(flight) {
+        this.flights.push(flight);
+    }
+
+    listFlights() {
+        return this.flights;
+    }
+
+    findFlightByCode(code) {
+        return this.flights.find(f => f.code === code);
+    }
+
+    deleteFlightByCode(code) {
+        var idx = this.flights.indexOf(this.flights.find(f => f.code === code));
+        this.flights.splice(idx,1);
+    }
+
+}
